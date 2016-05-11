1f5a54bf33e06f6b091a574a14dcd412cc347346
rich-farr <scarletmanuka@gmail.com>
2016-05-12 09:27:27 +1200

    add completed resume

--- js/helper.js
+++ js/helper.js
@@ -49,7 +49,7 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<div class="online-entry"></div>'; //'<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -178,6 +178,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -239,11 +240,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,73 +1,137 @@
 var bio = {
   "name": "Homer Simpson",
   "role": "Web Developer",
-  "bioPic": "http://images.gutefrage.net/media/fragen/bilder/wieso-sind-comic-figuren-immer-schwarz-umrandet/0_big.jpg",
-  "welcomeMsg": "Welcome to the internet my friend, what can I do for you?",
   "contacts":
     {
       "mobile": "939-555-0113",
       "email": "chunkylover53@aol.com",
       "github": "homer-jay",
-      "location": "Springfield, USA"
+      "location": "Springfield, MO, USA"
     },
+  "welcomeMessage": "Welcome to the internet my friend, what can I do for you?",
   "skills": [
     "Eating",
-    " Sleeping",
-    " Drinking",
-    " Watching TV",
-    " Strangling The Boy"]
+    "Sleeping",
+    "Drinking",
+    "Watching TV",
+    "Strangling The Boy"],
+  "bioPic": "http://images.gutefrage.net/media/fragen/bilder/wieso-sind-comic-figuren-immer-schwarz-umrandet/0_big.jpg"
 }
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+bio.display = function() {
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-$("#header").append(formattedBioPic);
-$("#header").append(formattedWelcomeMsg);
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+  var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+  $("#header").prepend(formattedRole);
+  $("#header").prepend(formattedName);
+  $("#header").append(formattedBioPic);
+  $("#header").append(formattedWelcomeMsg);
 
-$(".flex-box").append(formattedMobile);
-$(".flex-box").append(formattedEmail);
-$(".flex-box").append(formattedGithub);
-$(".flex-box").append(formattedLocation);
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
+  $(".flex-box").append(formattedMobile);
+  $(".flex-box").append(formattedEmail);
+  $(".flex-box").append(formattedGithub);
+  $(".flex-box").append(formattedLocation);
 
-if (bio.skills.length > 0) {
+  if (bio.skills.length > 0) {
 
-  $("#header").append(HTMLskillsStart);
-    for (var i = 0; i < bio.skills.length; i++) {
-      var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-      $("#skills").append(formattedSkill);
+    $("#header").append(HTMLskillsStart);
+      for (var i = 0; i < bio.skills.length; i++) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+      }
+  }
+}
+
+bio.display();
+
+var education = {
+  "schools": [
+    {
+      "name": "Springfield University",
+      "location": "Springfield, PA, USA",
+      "degree": "Sadgasm",
+      "majors": ["Rock", " Roll"],
+      "dates": "The 90s",
+      "url": "www.springfieldu.edu"
+    },
+    {
+      "name": "Springfield High School",
+      "location": "Springfield, TN, USA",
+      "degree": "The third degree, man",
+      "majors": ["Cruisin'", " Beer"],
+      "dates": "The 70s",
+      "url": "www.springfieldhs.edu"
     }
-};
+  ],
+  "onlineCourses": [
+    {
+      "title": "Outstanding Achievement in the Field of Excellence, Foundation Level",
+      "school": "C.M. Burns School of Excellence",
+      "date": "1992",
+      "url": "www.cmburnsschoolofexcellence.com"
+    }
+  ]
+}
+
+education.display = function() {
+    for (school in education.schools) {
+      $("#education").append(HTMLschoolStart);
+
+      var formattedName = HTMLschoolName.replace('%data%', education.schools[school].name);
+      var formattedLocation = HTMLschoolLocation.replace('%data%', education.schools[school].location);
+      var formattedDegree = HTMLschoolDegree.replace('%data%', education.schools[school].degree);
+      var formattedMajors = HTMLschoolMajor.replace('%data%', education.schools[school].majors);
+      var formattedDates = HTMLschoolDates.replace('%data%', education.schools[school].dates);
+
+      var formattedSchool = formattedName + formattedLocation + formattedDegree + formattedMajors + formattedDates;
+
+      $(".education-entry:last").append(formattedSchool);
+    }
+
+    for (online in education.onlineCourses) {
+      $("#online").append(HTMLonlineClasses);
+
+      var formattedTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[online].title);
+      var formattedNetSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[online].school);
+      var formattedDate = HTMLonlineDates.replace('%data%', education.onlineCourses[online].date);
+      var formattedURL = HTMLonlineURL.replace('%data%', education.onlineCourses[online].url);
+
+      var formattedOnline = formattedTitle + formattedNetSchool + formattedDate + formattedURL;
+
+      $(".online-entry:last").append(formattedOnline);
+    }
+}
+
+education.display();
 
 var work = {
   "jobs": [
     {
       "employer": "Springfield Nuclear Power Plant",
       "title": "Safety Inspector",
-      "location": "Springfield, USA",
+      "location": "Springfield, IL, USA",
       "dates": "1989 - 2016",
       "description": "Strengthened safety procedures that resulted in 75% fewer accidents on days I was absent. Pioneered workplace stress-reduction methods that worked for at least one employee."
     },
     {
       "employer": "City of Springfield",
       "title": "Chief of Police",
-      "location": "Springfield, USA",
+      "location": "Springfield, VA, USA",
       "dates": "2002",
       "description": "Broke up weasel-selling ring. Secured community support in almost-successful effort to expel organized crime from city."
     },
     {
       "employer": "Moe's Tavern",
       "title": "Bartender",
-      "location": "Springfield, USA",
+      "location": "Springfield, MA, USA",
       "dates": "2001",
       "description": "Engaged patrons in witty, whimsical, avuncular dialog covering topics such as the arts, philosophy, politics, and spectator sports. Launched remodeling project that led to 100% reduction in clientele (including myself)."
     },
@@ -81,21 +145,21 @@ var work = {
     {
       "employer": "Mr. Plow",
       "title": "Owner/Operator",
-      "location": "Springfield, USA",
+      "location": "Springfield, OR, USA",
       "dates": "1992 - 1993",
       "description": "Boosted business 15% by executing late-night TV marketing campaign targeting homeowners who were too wasted to shovel their driveways. Deliberated at length before rescuing man trapped beneath mountain avalanche."
     },
     {
       "employer": "Springfield Isotopes Baseball Club",
       "title": "Mascot ('Dancing Homer')",
-      "location": "Springfield, USA",
+      "location": "Springfield, KY, USA",
       "dates": "1990",
       "description": "Generated 25% increase in fan interest and 50% jump in beer sales by leading cheers at home games for minor league baseball team."
     }
   ]
 }
 
-function displayWork () {
+work.display = function() {
   for (job in work.jobs) {
     if (work.jobs.hasOwnProperty(job)) {
       $("#workExperience").append(HTMLworkStart);
@@ -113,7 +177,7 @@ function displayWork () {
   }
 }
 
-displayWork();
+work.display();
 
 var projects = {
   "projects": [
@@ -145,29 +209,26 @@ var projects = {
 }
 
 projects.display = function() {
-  $("#projects").append(HTMLprojectStart);
+  for (prop in projects.projects) {
+    if (projects.projects.hasOwnProperty(prop)) {
+      $("#projects").append(HTMLprojectStart);
+
+      var formattedTitle = HTMLprojectTitle.replace('%data%', projects.projects[prop].title);
+      var formattedDate = HTMLprojectDates.replace('%data%', projects.projects[prop].dates);
+      var formattedDescription = HTMLprojectDescription.replace('%data%', projects.projects[prop].description);
+      var formattedImage = HTMLprojectImage.replace('%data%', projects.projects[prop].images);
+
+      var formattedProject = formattedTitle + formattedDate + formattedDescription + formattedImage;
+
+      $(".project-entry:last").append(formattedProject);
+    }
+  }
 }
 
-var education = {
-  "schools": [
-    {
-      "name": "Springfield High School",
-      "location": "Springfield, USA",
-      "degree": "None",
-      "majors": ["None", "None"],
-      "dates": "The 70s",
-      "url": "springfieldhs.edu"
-    }
-  ],
-  "onlineCourses": [
-    {
-      "title": "",
-      "school": "",
-      "dates": "",
-      "url": ""
-    }
-  ]
-}
+projects.display();
+
+
+$("#mapDiv").append(googleMap);
 
 // $(document).click(function(loc) {
 //     var x = loc.pageX;
@@ -176,13 +237,13 @@ var education = {
 //     logClicks(x,y);
 // })
 
-function inName(name) {
-  name = name.trim().split(" ");
-  name[1] = name[1].toUpperCase();
-  name[0] = name[0].slice(0,1).toUpperCase() + 
-    name[0].slice(1).toLowerCase();
+// function inName(name) {
+//   name = name.trim().split(" ");
+//   name[1] = name[1].toUpperCase();
+//   name[0] = name[0].slice(0,1).toUpperCase() + 
+//     name[0].slice(1).toLowerCase();
 
-    return name[0] +" "+ name[1];
-}
+//     return name[0] +" "+ name[1];
+// }
 
-$("#main").append(internationalizeButton);
+// $("#main").append(internationalizeButton);
